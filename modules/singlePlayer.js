b66ed65d3c760201f9fc019f13b589a7b2991a40
Simon Dutka <SimonDutkaWork@gmail.com>
2023-08-12 22:36:25 +0200

    Added basic validation to 'pieceChoice'
    -Added comments

--- modules/singlePlayer.js
+++ modules/singlePlayer.js
@@ -2,7 +2,7 @@ import { input } from '@inquirer/prompts';
 
 import showBoard from './board.js';
 
-let lettersValue = {
+let squaresValues = {
     1: 0,
     2: 8,
     3: 16,
@@ -21,22 +21,50 @@ let lettersValue = {
     H: 7,
 };
 
+let squareKeysNum = Object.keys(squaresValues);
+
+squareKeysNum = squareKeysNum
+    .filter((el) => !isNaN(el))
+    .map((x) => parseInt(x, 10));
+
 const singlePlayer = () => {
     showBoard();
 
     const pickPiece = async () => {
-        let pieceChoice = await input({
-            message: 'Select piece',
-        });
+        // Get piece to move
+        const getPiece = async () => {
+            let validateStatus = true;
+            do {
+                validateStatus = true;
+                let pieceChoice = await input({
+                    message: 'Select piece',
+                });
 
-        let pickedPiece = pieceChoice
+                // If first is not a letter
+                if (squareKeysNum.includes(parseInt(pieceChoice[0]))) {
+                    validateStatus = false;
+                }
+
+                if (pieceChoice.length !== 2) validateStatus = false;
+                console.log(validateStatus);
+            } while (validateStatus === false);
+        };
+
+        getPiece();
+
+        let pickedPiece = pick
+            // Upper letter
             .toUpperCase()
+            // Make an array
             .split('')
             .map((el) => {
-                el = lettersValue[el];
+                // Get value of picked piece
+                el = squaresValues[el];
+                // Return elements as numbers
                 return parseInt(el, 10);
             });
 
+        // Add value to get correct piece
         pickedPiece = pickedPiece[0] + pickedPiece[1];
     };
 
