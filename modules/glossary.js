53b6d576f192f6ccebc27c277160db57e4bb2e4e
Simon Dutka <89161825+simon-dutka@users.noreply.github.com>
2023-08-18 23:09:33 +0200

    Updated glossary

--- modules/glossary.js
+++ modules/glossary.js
@@ -1,12 +1,17 @@
 const showGlossary = () => {
-    let glossary = `Pieces names:
+    let glossary = `
+Pieces names:
         K - King
         Q - Queen
         N - Knight
         B - Bishop
         R - Rook
         p - Pawn
-        x - Empty square
+        • - Empty square
+
+How to play:
+        1. Select piece wchich you want to move
+        2. Select square you want to move selected pawn
 `;
     process.stdout.write(glossary);
 };
