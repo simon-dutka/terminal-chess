9cbe78db51d9105a9717961f2f4a7f7fe1f8a363
Simon Dutka <Szymon.dutka@outlook.com>
2023-10-26 16:56:44 +0200

    Added switch case

--- modules/menu.js
+++ modules/menu.js
@@ -1,5 +1,6 @@
 import { select } from '@inquirer/prompts';
 import singlePlayer from '../modules/singlePlayer.js';
+import engines from '../modules/engine.js';
 
 const menu = async () => {
     let menuChoice = await select({
@@ -19,16 +20,22 @@ const menu = async () => {
             },
         ],
     });
-    if (menuChoice === 0) {
-        console.clear();
-        singlePlayer();
-    }
-    if (menuChoice === 1) {
-        console.clear();
-    }
-    if (menuChoice === 2) {
-        console.clear();
-        process.exit();
+
+    switch (menuChoice) {
+        case 0:
+            console.clear();
+            singlePlayer();
+            break;
+        case 1:
+            console.clear();
+            engines();
+            break;
+        case 2:
+            console.clear();
+            process.exit();
+            break;
+        default:
+            break;
     }
 };
 
