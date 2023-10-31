e2ae9b138c8ccbe7955e582c79118096f6a3f66f
Simon Dutka <Szymon.dutka@outlook.com>
2023-10-31 13:40:14 +0100

    Moved getting piece to engine.js
    -Updated validation for user picks

--- modules/engine.js
+++ modules/engine.js
@@ -1,17 +1,110 @@
-// Pawn, Knight, Bishoop, Rook, Queen
-let piecesNames = ['p', 'n', 'b', 'r', 'q'];
-let piecesValues = [1, 3, 3, 5, 9];
-let pieces = [];
+import { input } from '@inquirer/prompts';
+import { showBoard, defaultBoard } from './board.js';
 
-let piece = {
-    name: 'King',
-    pieceValue: '0',
+let squaresValues = {
+    1: 0,
+    2: 8,
+    3: 16,
+    4: 24,
+    5: 32,
+    6: 40,
+    7: 48,
+    8: 56,
+    a: 0,
+    b: 1,
+    c: 2,
+    d: 3,
+    e: 4,
+    f: 5,
+    g: 6,
+    h: 7,
 };
 
-for (let i = 0; i < 4; i++) {
-    let el = Object.create(piece);
-    el.name = piecesNames[i];
-    el.value = piecesValues[i];
+let squaresKeysNum = Object.keys(squaresValues);
+let squaresKeysStr = Object.keys(squaresValues);
 
-    pieces.push(el);
-}
+squaresKeysNum = squaresKeysNum
+    .filter((el) => !isNaN(el))
+    .map((x) => parseInt(x, 10));
+
+squaresKeysStr = squaresKeysStr.filter((el) => isNaN(el)).map((x) => x);
+
+const engine = async () => {
+    let validationStatus, pickedPiece, pickedMove;
+
+    // ToDo:  -check if square is not empty; -check if move is allowed -check if picked square is not the same square
+
+    const defaultValidation = (answer) => {
+        // ToDo: separate syntax error and wrong numbers/letters selected
+        if (
+            squaresKeysStr.includes(answer[0]) === false ||
+            squaresKeysNum.includes(Number(answer[1])) === false ||
+            answer.length < 2 ||
+            answer.length > 2
+        ) {
+            console.log('Wrong syntax! Right syntax example: a1');
+            validationStatus = false;
+        } else {
+            validationStatus = true;
+        }
+    };
+
+    const movePieceValidation = (answer) => {
+        defaultValidation(answer);
+        if (pickedPiece === answer) {
+            console.log('Piece to move cannot be current pawn piece');
+            validationStatus = false;
+        }
+    };
+
+    // Get piece to move
+    const getPiece = async () => {
+        do {
+            pickedPiece = await input({
+                message: 'Select piece',
+            });
+
+            defaultValidation(pickedPiece);
+        } while (validationStatus === false);
+    };
+
+    const movePiece = async () => {
+        do {
+            pickedMove = await input({
+                message: 'Move to',
+            });
+
+            movePieceValidation(pickedMove);
+        } while (validationStatus === false);
+    };
+
+    const getValueOfPiece = async (itemToGetValue) => {
+        let item = itemToGetValue
+            // Upper letter
+            .toUpperCase()
+            // Make an array
+            .split('')
+            .map((el) => {
+                // Get value of picked piece
+                el = squaresValues[el];
+                // Return elements as numbers
+                return parseInt(el, 10);
+            });
+
+        // Add value to get correct piece
+        item = itemToGetValue[0] + itemToGetValue[1];
+    };
+
+    let id = 0;
+    const movePawn = async () => {
+        let item = defaultBoard.boardPositions[id];
+        defaultBoard.boardPositions.splice(id, 1, '·');
+        defaultBoard.boardPositions.splice(id + 8, 1, item);
+    };
+
+    await getPiece();
+    await movePiece();
+    await movePawn(pickedPiece, pickedMove);
+};
+
+export default engine;
--- modules/singlePlayer.js
+++ modules/singlePlayer.js
@@ -1,97 +1,11 @@
 import { input } from '@inquirer/prompts';
 
-import { showBoard, defaultBoard } from './board.js';
-
-let squaresValues = {
-    1: 0,
-    2: 8,
-    3: 16,
-    4: 24,
-    5: 32,
-    6: 40,
-    7: 48,
-    8: 56,
-    A: 0,
-    B: 1,
-    C: 2,
-    D: 3,
-    E: 4,
-    F: 5,
-    G: 6,
-    H: 7,
-};
-
-let squaresKeysNum = Object.keys(squaresValues);
-let squaresKeysStr = Object.keys(squaresValues);
-
-squaresKeysNum = squaresKeysNum
-    .filter((el) => !isNaN(el))
-    .map((x) => parseInt(x, 10));
-
-squaresKeysStr = squaresKeysStr.filter((el) => isNaN(el)).map((x) => x);
+import engine from './engine.js';
 
 const singlePlayer = () => {
-    showBoard();
-
-    let pickedPiece, pickedMove;
-
-    const pickPiece = async () => {
-        let validateStatus;
-
-        const validation = (answer) => {
-            if (
-                // If first is not a letter
-                squaresKeysNum.includes(parseInt(answer[0])) ||
-                answer.length !== 2 ||
-                squaresKeysStr.includes(answer[1])
-            ) {
-                validateStatus = false;
-            }
-        };
-
-        // Get piece to move
-        const getPiece = async () => {
-            do {
-                validateStatus = true;
-                pickedPiece = await input({
-                    message: 'Select piece',
-                });
-
-                validation(pickedPiece);
-            } while (validateStatus === false);
-        };
-
-        const movePiece = async () => {
-            do {
-                validateStatus = true;
-                pickedMove = await input({
-                    message: 'Move to',
-                });
-
-                validation(pickedMove);
-            } while (validateStatus === false);
-        };
-
-        await getPiece();
-        await movePiece();
-
-        pickedPiece = pickedPiece
-            // Upper letter
-            .toUpperCase()
-            // Make an array
-            .split('')
-            .map((el) => {
-                // Get value of picked piece
-                el = squaresValues[el];
-                // Return elements as numbers
-                return parseInt(el, 10);
-            });
-
-        // Add value to get correct piece
-        pickedPiece = pickedPiece[0] + pickedPiece[1];
-    };
-
-    pickPiece();
+    engine();
 };
+
+singlePlayer(); //! Test log
 
 export default singlePlayer;
