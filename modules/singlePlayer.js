f9cbba4b48024c4d0e0f697686438cddc1af1957
Simon Dutka <Szymon.dutka@outlook.com>
2023-11-04 21:10:41 +0100

    Updated import names from board.js file

--- modules/singlePlayer.js
+++ modules/singlePlayer.js
@@ -1,8 +1,9 @@
 import { input } from '@inquirer/prompts';
-
+import { showBoard, board } from './board.js';
 import engine from './engine.js';
 
 const singlePlayer = () => {
+    showBoard();
     engine();
 };
 
